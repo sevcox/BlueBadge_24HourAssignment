150a37a63d762a0c12534ff489fd7d22967b8c46
SperZ <ZachSperka@gmail.com>
2020-07-28 09:48:03 -0400

    7/28/2020 9:47am

---
+++ SocialMedia.Models/CommentListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class CommentListItem
+    {
+        public int CommentId { get; set; }
+        [Display(Name= "Author")]
+        public Guid OwnerId { get; set; }
+        [Display(Name = "Comment")]
+        public string Text { get; set; }
+        public int PostId { get; set; }
+    }
+}
