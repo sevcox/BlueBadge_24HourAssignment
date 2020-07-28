8aaddaf6395032cffbf80d1eee238d079ae6e945
tylerklink75 <66278928+tylerklink75@users.noreply.github.com>
2020-07-28 10:19:48 -0400

    git

--- SocialMedia.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SocialMedia.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- SocialMedia.WebAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SocialMedia.WebAPI.WebApiApplication" Language="C#" %>
