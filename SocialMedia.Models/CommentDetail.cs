38a4430187a7d33c17c1fc5006f8aabf4be74b78
Severa <severa.cox@gmail.com>
2020-07-28 09:47:49 -0400

    changes to model

---
+++ SocialMedia.Models/CommentDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class CommentDetail
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        [Display(Name = "Author")]
+        public Guid OwnerId { get; set; }
+        public int PostId { get; set; }
+    }
+}
---
+++ SocialMedia.Models/ReplyDetail.cs
@@ -0,0 +1,14 @@
+﻿using SocialMedia.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class ReplyDetail
+    {
+        public Comment ReplyComment { get; set; }
+    }
+}
