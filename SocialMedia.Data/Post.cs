a5d5ae5de492af1cec73a0ec8e8f7f7048167c76
tylerklink75 <66278928+tylerklink75@users.noreply.github.com>
2020-07-28 10:22:53 -0400

    adding git ignore

---
+++ SocialMedia.Data/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Data
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        [Display(Name = "Author")]
+        public Guid OwnerId { get; set; }
+    }
+}
