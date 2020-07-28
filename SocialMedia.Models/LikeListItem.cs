56c0a8eaa81c5c477ecce49aa7fe7706f762c9e2
Severa <severa.cox@gmail.com>
2020-07-28 13:16:00 -0400

    added like and reply services

---
+++ SocialMedia.Models/LikeListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class LikeListItem
+    {
+        [Display(Name = "Liker")]
+        public Guid OwnerId { get; set; }
+    }
+}
--- SocialMedia.Services/LikeService.cs
+++ SocialMedia.Services/LikeService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SocialMedia.Data;
+using SocialMedia.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,79 @@ namespace SocialMedia.Services
 {
     public class LikeService
     {
+        private readonly Guid _userId;
+
+        public LikeService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateLike(LikeCreate model)
+        {
+            var entity =
+                new Like()
+                {
+                    OwnerId = _userId,
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Likes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+
+        }
+        public IEnumerable<LikeListItem> GetLikes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Likes
+                    .Select(
+                        e =>
+                        new LikeListItem
+                        {
+                            OwnerId = e.OwnerId
+                        }
+                        );
+
+                return query.ToArray();
+            }
+        }
+
+        public LikeDetail GetLikeById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                     .Likes
+                     .Single(e => e.PostId == id);
+                return
+                 new LikeDetail
+                 {
+                     LikeId = entity.LikeId,
+                     PostId = entity.PostId,
+                     OwnerId = entity.OwnerId
+                 };
+
+            }
+        }
+
+        public bool DeleteLike(int likeId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Likes
+                    .Single(e => e.LikeId == likeId);
+
+                ctx.Likes.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
     }
 }
