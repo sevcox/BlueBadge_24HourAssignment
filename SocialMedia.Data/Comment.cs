d9f10b6daf0a92213e96a024aae688de657e980f
Severa <severa.cox@gmail.com>
2020-07-27 23:29:59 -0400

    first commit

---
+++ SocialMedia.Data/Comment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Data
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        [Display(Name ="Author")]
+        public Guid OwnerId { get; set; }
+        [Required]
+        [Display(Name ="CommentPost")]
+        public int PostId { get; set; }
+        [ForeignKey(nameof(PostId))]
+        public virtual Post Post { get; set; }
+    }
+}
---
+++ SocialMedia.Data/Like.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Data
+{
+    public class Like
+    {
+        [Key]
+        public int LikeId { get; set; }
+        [Required]
+        public int? PostId { get; set; }
+        [ForeignKey(nameof(PostId))]
+        public virtual Post Post { get; set; }
+        [Required]
+        [Display(Name = "Liker")]
+        public Guid OwnerId { get; set; }
+    }
+}
